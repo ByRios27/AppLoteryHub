44609f95ec2c6e31e23c423a45618b312cbdd979
jrios5061 <jrios5061@gmail.com>
2025-09-30 18:04:17 +0000

    Fix icon map keys to lowercase

--- src/lib/icon-map.ts
+++ src/lib/icon-map.ts
@@ -1,14 +1,14 @@
 import { Clover, Diamond, Sun, Moon, Star, Crown, Gem, Hash, Dice5, Ticket } from 'lucide-react';
 
 export const iconMap = {
-  Clover,
-  Diamond,
-  Sun,
-  Moon,
-  Star,
-  Crown,
-  Gem,
-  Hash,
-  Dice5,
-  Ticket,
+  clover: Clover,
+  diamond: Diamond,
+  sun: Sun,
+  moon: Moon,
+  star: Star,
+  crown: Crown,
+  gem: Gem,
+  hash: Hash,
+  dice5: Dice5,
+  ticket: Ticket,
 };
